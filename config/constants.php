2ca95ffd2ae831bc5fde99e2f9f6fa149a3ddbe8
Matthew Toledo <mrbinky3000@gmail.com>
2011-06-01 13:43:08 -0400

    first commit

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * The reCAPTCHA server URL's
+ */
+define("RECAPTCHA_API_SERVER", "http://www.google.com/recaptcha/api");
+define("RECAPTCHA_API_SECURE_SERVER", "https://www.google.com/recaptcha/api");
+define("RECAPTCHA_VERIFY_SERVER", "www.google.com");
+
